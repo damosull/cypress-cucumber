3ea91b00ed8b55b09993f1a5c03c3271a352a87d
Damien O'Sullivan <dosullivan@glasslewis.com>
2023-08-16 13:48:37 +0100

    removing more old stuff

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,11 @@ const { defineConfig } = require('cypress');
 const { cloudPlugin } = require('cypress-cloud/plugin');
 const fs = require('fs-extra');
 const xlsx = require('node-xlsx').default;
-const { readPdf } = require('./cypress/utils/read-pdf');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const preprocessor = require('@badeball/cypress-cucumber-preprocessor');
 const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 const stdLibBrowser = require('node-stdlib-browser');
 const plugin = require('node-stdlib-browser/helpers/esbuild/plugin');
-const { queryTestDb } = require('./cypress/utils/queryDB');
 
 async function setupNodeEvents(on, config) {
 	config.baseUrl = 'https://the-internet.herokuapp.com';
@@ -46,10 +44,6 @@ async function setupNodeEvents(on, config) {
 	});
 
 	on('task', {
-		readPdf: (pathToPdf) => readPdf(pathToPdf),
-	});
-
-	on('task', {
 		parseXlsx({ filePath, sheetName, rows }) {
 			return new Promise((resolve, reject) => {
 				try {
@@ -59,12 +53,6 @@ async function setupNodeEvents(on, config) {
 					reject(e);
 				}
 			});
-		},
-	});
-
-	on('task', {
-		queryDb: (query) => {
-			return queryTestDb(query, config);
 		},
 	});
 
--- cypress/utils/date.js
+++
@@ -1,9 +0,0 @@
-const dayjs = require('dayjs');
-
-export function convertDate(dateInput, dateFormat) {
-	return dayjs(dateInput).format(dateFormat);
-}
-
-export function getCurrentTime() {
-	return dayjs().toISOString();
-}
